3fd91b26d8c3b12aa20a9f527924e02eecb0014f
Furkancan Turksoy <furkanturksoy@windowslive.com>
2017-02-08 09:58:40 +0200

    Added UI

---
+++ Finale.UI/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Finale.UI.Controllers
+{
+    [Route("Categories")]
+    public class CategoryController : Controller
+    {
+        [Route("")]
+        public IActionResult CategoryList()
+        {
+            return View();
+        }
+
+        [Route("{name}")]
+        public IActionResult CategoryByName(string name)
+        {
+            return View(name);
+        }
+    }
+}
