7f8784a7c5c0547d6f53d23ac397c2529f8325ce
Furkancan Turksoy <furkanturksoy@windowslive.com>
2017-02-21 11:52:39 +0200

    added register animation

--- Finale.UI/content/ajax/registerAjax.js
+++ Finale.UI/content/ajax/registerAjax.js
@@ -1,1 +1,34 @@
-﻿
+﻿function initializeRegister(loader, loaderClose, successMessage) {
+
+
+    var registerloader = document.getElementById("register-loader");
+    var loaderContent = document.getElementById("register-loader-content");
+    var registerSuccess = document.getElementById("register-success");
+    var registerForm = document.getElementById("register-modal-content");
+
+    $("#register-button").click(function (e) {
+        e.preventDefault();
+        var Data = {
+            Username: document.getElementById("username").value,
+            Password: document.getElementById("password").value
+        }
+        $.ajax({
+            type: 'POST',
+            url: 'api/register',
+            data: Data,
+            async: true,
+            beforeSend: loader(registerloader, loaderContent, registerForm),
+            success: function (data) {
+                if (data) {
+                    setTimeout(function () { loaderClose(registerloader, loaderContent, registerForm) }, 800);
+                    setTimeout(function () { successMessage(registerSuccess) }, 1000);
+
+
+                }
+
+
+            }
+        })
+    })
+}
+
